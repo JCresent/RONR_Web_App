5ce866b7b559fcad0f31e8cfad1c7da9515fa62f
Justin Cresent <JCresent@hotmail.com>
2024-12-03 21:44:57 -0500

    Added check to make sure input passwords match

--- ronr_web_app/src/components/login-pages/registration.jsx
+++ ronr_web_app/src/components/login-pages/registration.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login-styles.css';
 import registerIcon from '../../icons/registration_icon.svg';
 
 const RegistrationPage = () => {
+
+    // Define state variables for form input and errors 
+    const [formInput, setFormInput] = useState({
+        email: "",
+        psw: "",
+        confirmPassword: "", 
+    });
+
+    const [formError, setFormError] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const handleUserInput = (name, value) => {
+        setFormInput({
+            ...formInput,
+            [name]: value,
+        });
+    };
+
+    const validateFormInput = (event) => {
+        console.log("Validating input"); 
+        event.preventDefault(); 
+
+        // Track input errors
+        let inputError = {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        }; 
+
+        //Check if passwords match
+        if (formInput.confirmPassword !== formInput.psw) {
+            console.log("Don't match!");
+            setFormError({
+                ...inputError,
+                confirmPassword: "Passwords should match",
+            });
+            return false;
+        }
+    }
 
     return (
         <div>
@@ -13,7 +55,6 @@ const RegistrationPage = () => {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>Registration page</title>
             <link rel="stylesheet" href="login-styles.css" />
-            <form action="/action_page.php">
                 <div className="container">
                 <div className="registration-title">
                     <h1 className="registration-item">Register</h1>
@@ -22,13 +63,19 @@ const RegistrationPage = () => {
                 <br />
                 <br />
                 <br />
-                <form action="../../../../newuser/post" method="post" className="reg_form">
+                <form onSubmit={validateFormInput} action="../../../../newuser/post" method="post" className="reg_form">
                     <label htmlFor="email" style={{ display: 'block', textAlign: 'left'}}><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" required />
+                    <input value={formInput.email} onChange={({ target }) => {handleUserInput(target.name, target.value)} } type="text" placeholder="Enter Email" name="email" id="email" required />
+                    <p className="error-message">{formError.email}</p>
+
                     <label htmlFor="psw" style={{ display: 'block', textAlign: 'left'}}><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
+                    <input value={formInput.psw} onChange={({ target }) => { handleUserInput(target.name, target.value)} } type="password" placeholder="Enter Password" name="psw" id="psw" required />
+                    <p className="error-message">{formError.password}</p>
+
                     <label htmlFor="psw-repeat" style={{ display: 'block', textAlign: 'left'}}><b>Re-enter Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
+                    <input value={formInput.confirmPassword} onChange={({ target }) => { handleUserInput(target.name, target.value)} }  type="password" placeholder="Repeat Password" name="confirmPassword" id="psw-repeat" required />
+                    <p className="error-message">{formError.confirmPassword}</p>
+
                     <button type="submit" className="registerbtn">Register</button>
                 </form>
                 <br />
@@ -37,7 +84,6 @@ const RegistrationPage = () => {
                         <Link to="/" >Already have an account?</Link>
                     </div>
                 </div>
-            </form>
         </div>
 
 
